353fa2ea5757dc2dec623d28912c25118a0ee31c
ssjeong <ssjeong920801@gmail.com>
2022-12-13 13:19:27 +0900

    [Python] 파이썬 숫자 자료형

---
+++ number.py
@@ -0,0 +1,42 @@
+# 2022.12.13[프뚜]: 정수
+integer = 10
+print(integer)
+
+integer = -10
+print(integer)
+
+# 2022.12.13[프뚜]: 실수
+floatingPoint = 10.5
+print(floatingPoint)
+
+floatingPoint = -10.5
+print(floatingPoint)
+
+# 2022.12.13[프뚜]: 8진수
+octal = 0o17
+print(octal)
+
+# 2022.12.13[프뚜]: 16진수
+hexadecimal = 0x8f
+print(hexadecimal)
+
+# 2022.12.13[프뚜]: 사칙연산
+x = 5
+y = 3
+
+x + y
+x - y
+x * y
+x / y
+
+# 2022.12.13[프뚜]: 제곱
+x ** y 
+
+# 2022.12.13[프뚜]: 나눗셈 후 나머지 값
+x % y
+
+# 2022.12.13[프뚜]: 나눗셈 후 몫 값
+x // y 
+
+
+
