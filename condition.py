4be9b0ede89d416d0b5e80300293b30099445e5c
ssjeong <ssjeong920801@gmail.com>
2022-12-14 11:27:40 +0900

    [Python] 파이썬 조건문 (if)

---
+++ condition.py
@@ -0,0 +1,23 @@
+
+age = 20
+if 10 <= age < 20:
+    print("10 ~ 19살 이상 입니다.")
+elif 20 <= age < 30:
+    print("20 ~ 29살 입니다.")
+else:
+    print("30살 이상 입니다.")
+
+array = [1, 2, 3]
+if 1 in array:
+    print("1을 포함했습니다.")
+else:
+    print("1을 포함하지 않습니다.")
+
+name = "프뚜"
+if "프뚜" == name:
+    print("이름은 프뚜입니다.")
+else:
+    print("이름은 프뚜가 아닙니다.")
+
+
+    
